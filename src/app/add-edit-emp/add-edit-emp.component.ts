711183991d988b26671e06eb3f430d072ab592c0
CHAUDHARI MAHIMA SURYAKANT <99661594+Mahima-2001@users.noreply.github.com>
2024-05-21 15:34:08 +0530

    angular

---
+++ src/app/add-edit-emp/add-edit-emp.component.ts
@@ -0,0 +1,64 @@
+import { Component, Inject, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup } from '@angular/forms';
+import { ServiceService } from '../shared/service.service';
+import { MAT_DIALOG_DATA, MatDialogRef } from '@angular/material/dialog';
+
+@Component({
+  selector: 'app-add-edit-emp',
+  templateUrl: './add-edit-emp.component.html',
+  styleUrls: ['./add-edit-emp.component.css']
+})
+export class AddEditEmpComponent implements OnInit {
+
+  empForm:FormGroup;
+  education:string[]=['Matric','Diploma','Intermediate','Greduate','Post Greduate']
+
+  constructor(private fb:FormBuilder, private service:ServiceService, private dialogref:MatDialogRef<AddEditEmpComponent>,
+    @Inject (MAT_DIALOG_DATA) public data:any){
+    this.empForm = this.fb.group({
+      firstname:'',
+      lastname:'',
+      email:'',
+      dob:'',
+      gender:'',
+      education:'',
+      company:'',
+      exp:'',
+      package:''
+    })
+  }
+  ngOnInit(): void {
+    this.empForm.patchValue(this.data)
+  }
+
+  onFormSubmit()
+  {
+    if(this.empForm.valid)
+    {
+      if(this.data)
+      {
+        this.service.updateEmployee(this.data.id,this.empForm.value).subscribe({
+          next :(val:any)=>{
+            alert('Employee Details Updated Sucessfully..!!')
+            this.dialogref.close(true)
+          },
+          error:(err:any)=>{
+            alert(err)
+          }
+        })
+      }
+      else
+      {
+        this.service.addEmployee(this.empForm.value).subscribe({
+          next :(val:any)=>{
+            alert('Employee Added Sucessfully..!!')
+            this.dialogref.close(true)
+          },
+          error:(err:any)=>{
+            alert(err)
+          }
+        })
+      }
+    }
+  }
+}
